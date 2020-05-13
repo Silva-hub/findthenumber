1b74c8c393528375cf9ef2adfac69f53167546e1
Xhaferllari, Alban <albax@amazon.com>
2020-05-13 21:33:30 +0100

    Random number generation functions

--- js/app.js
+++ js/app.js
@@ -3,20 +3,27 @@ const cellsInRow = 10;
 const min = 1;
 const max = 100;
 
+function generateRandomNumbers(max = 100) {
+    return Array(max)
+        .fill()
+        .map((_, i) => i + 1)
+        .sort(() => Math.random() - 0.5);
+}
+
 function drawTable() {
     // get the reference for the body
     const randomnum1 = document.querySelector('.random-num');
 
     // creates a <table> element
-    const tbl = document.createElement("table");
+    const tbl = document.createElement('table');
 
     // creating rows
     for (let r = 0; r < totalRows; r++) {
-        const row = document.createElement("tr");
+        const row = document.createElement('tr');
 
         // create cells in row
         for (let c = 0; c < cellsInRow; c++) {
-            let cell = document.createElement("td");
+            let cell = document.createElement('td');
             getRandom = Math.floor(Math.random() * (max - min + 1)) + min;
             let cellText = document.createTextNode(Math.floor(Math.random() * (max - min + 1)) + min);
             cell.appendChild(cellText);
